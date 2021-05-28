e04e9b93a0e6d7c4147b870ab5238c7a6882d050
IvannaZobniv <ivannazobniv333@gmail.com>
2021-05-28 15:42:19 +0300

    new

--- FirstTeamProject/FirstTeamProject/UnitTest1.cs
+++ FirstTeamProject/FirstTeamProject/UnitTest1.cs
@@ -1,22 +1,38 @@
 using NUnit.Framework;
+using TechTalk.SpecFlow;
 
 namespace FirstTeamProject
 {
+    [Binding]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Given(@"the first number is 50")]
+        public int FirstNumber()
         {
-
+            var firstNumber = 50;
+            return firstNumber;
         }
 
-        [Test]
-        public void Test1()
+        [Given(@"the second number is 70")]
+        public int SecondNumber()
         {
-            var nice = 3;
-            nice += 1;
-            
-            Assert.AreEqual(4, nice);
+            var secondNumber = 70;
+            return secondNumber;
         }
+
+        [When(@"the two numbers are added")]
+        public int Sum()
+        {
+            var firstNumber = FirstNumber();
+            var secondNumber = SecondNumber();
+            return firstNumber + secondNumber;
+        }
+        [Then(@"the result should be 120")]
+        public void Is120()
+        {
+            var expectedResult = 120;
+            Assert.AreEqual(expectedResult, Sum());
+        }
+
     }
 }
